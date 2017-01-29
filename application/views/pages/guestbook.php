eabbd2584c9008eeabd6001fe87f21f088a89342
Richard <lapislacrima@gmail.com>
2017-01-29 07:24:53 -0800

    add time, classes and semantic corrections
    more semantic tags were applied to elements
    most elements now have a class for easier application of css
    time of comment is now shown

--- application/views/pages/guestbook.php
+++ application/views/pages/guestbook.php
@@ -1,15 +1,24 @@
 
-<ul>
+<section class="guestbook-entries">
 <?php
 	foreach($guestlist->result_array() as $value) {
 		$v_name = $value['visitor'];
 		$v_comment = $value['comment'];
+		$v_time = date(DATE_RFC2822, strtotime($value['time']));
 		$comment_id = $value['id'];
-		echo "<li>";
-		echo "Guest $v_name says $v_comment";
-		echo "<a href='guestbook/delete_comment/$comment_id'> Delete </a>";
-		echo "</li>";
+		echo "
+		<article class='guestbook-entry'>
+			<span class='visitor-name'> $v_name says:</span>
+			<br/>
+			<span class='comment-time'> at $v_time </span>
+			<div class='para-container'>
+				<a href='guestbook/delete_comment/$comment_id'> <i class='fa fa-trash' aria-hidden='true'></i> </a>
+				<p class='visitor-comment'> $v_comment </p>
+			</div>
+		</article>
+		";
+
 	}
 ?>
-</ul>
+</section>
 
