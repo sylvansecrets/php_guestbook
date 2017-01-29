9c72455aaf5be0c55337d07624ec8f74c1cbf174
Richard <lapislacrima@gmail.com>
2017-01-29 00:53:12 -0800

    add delete link

--- application/views/pages/guestbook.php
+++ application/views/pages/guestbook.php
@@ -4,7 +4,11 @@
 	foreach($guestlist->result_array() as $value) {
 		$v_name = $value['visitor'];
 		$v_comment = $value['comment'];
-		echo "<li> Guest $v_name says $v_comment </li>";
+		$comment_id = $value['id'];
+		echo "<li>";
+		echo "Guest $v_name says $v_comment";
+		echo "<a href='guestbook/delete_comment/$comment_id'> Delete </a>";
+		echo "</li>";
 	}
 ?>
 </ul>
