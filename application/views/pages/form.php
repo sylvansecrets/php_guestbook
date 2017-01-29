335f91245d89979dc80d2ec0924c8c1183d6efb0
Richard <lapislacrima@gmail.com>
2017-01-29 07:23:35 -0800

    encase in section tag; add classes
    most elements now have a class for eaiser application of css

--- application/views/pages/form.php
+++ application/views/pages/form.php
@@ -1,39 +1,43 @@
-<?php
+<section class="form-submission">
 
-echo form_open('guestbook/data_submitted/'.$anti_bot);
+	<?php
 
-echo form_label('Visitor Name: ', 'v_name');
+	echo form_open('guestbook/data_submitted/'.$anti_bot, 'class="form-content"');
 
-$data = array(
-	'name' => 'v_name',
-	'placeholder' => 'Please Enter Your Name',
-	'class' => 'name_box'
-	);
+	echo form_label('Visitor Name: ', 'v_name');
 
-echo form_input($data);
-
-echo "<br/>";
-
-echo form_label('Visitor Comment: ', 'v_comment');
-$data = array(
-	'name' => 'v_comment',
-	'placeholder' => 'Leave a message in the guestbook!',
-	'class' => 'v_comment'
-	);
-
-echo form_input($data);
-
-?>
-
-<div class="form_button">
-<?php
 	$data = array(
-		'type' => 'submit',
-		'value' => 'Submit',
-		'class' => 'submit_button'
+		'name' => 'v_name',
+		'placeholder' => 'Please Enter Your Name',
+		'class' => 'name-box'
 		);
-	echo form_submit($data);
-?>
-</div>
 
-<?php echo form_close(); ?>
+	echo form_input($data);
+
+	echo "<br/>";
+
+	echo form_label('Visitor Comment: ', 'v_comment');
+	$data = array(
+		'name' => 'v_comment',
+		'placeholder' => 'Leave a message in the guestbook!',
+		'class' => 'v-comment',
+		'type' => 'text'
+		);
+
+	echo form_textarea($data);
+
+	?>
+
+	<div class="form_button">
+	<?php
+		$data = array(
+			'type' => 'submit',
+			'value' => 'Submit',
+			'class' => 'submit_button'
+			);
+		echo form_submit($data);
+	?>
+	</div>
+
+	<?php echo form_close(); ?>
+</section>
