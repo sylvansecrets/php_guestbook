9ea829df12839db253eb43ef15c3ca2148e3722f
Richard <lapislacrima@gmail.com>
2017-01-29 07:26:00 -0800

    include stylesheets; add main tag;
    reset and font-awesome as well as custom css now loaded
    main tag now added for semantic reasons

--- application/views/pages/header.php
+++ application/views/pages/header.php
@@ -2,7 +2,14 @@
 <html>
 <head>
 	<title>GuestBook</title>
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/meyer-reset/2.0/reset.css" integrity="sha256-7VVaJ5GDwFQiLIc+eNksQLUSSY5JNZtqv9o2BI8UGYg=" crossorigin="anonymous" />
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"/>
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>css/style.css"/>
 </head>
 <body>
 
-<p>Guestbook Starts Here</p>
+<div class="banner">
+	<h2 class='page-header-title'> Guestbook </h2>
+</div>
+
+<main>
